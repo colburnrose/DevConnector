d7374e973cd4b4c93da7f6375243a008fdbe46f2
Colburn Sanders <colburnsanders@gmail.com>
2019-05-19 15:20:17 -0500

    added User model & Request/Body Validation

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 //use esversion: 6
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect to DB
+connectDB();
+
+// Init Middleware
+app.use(express.json({
+    extended: false
+}));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./Routes/Api/users'));
+app.use('/api/auth', require('./Routes/Api/auth'));
+app.use('/api/profile', require('./Routes/Api/profile'));
+app.use('/api/post', require('./Routes/Api/post'));
+
 
 const PORT = process.env.PORT || 5000;
 
