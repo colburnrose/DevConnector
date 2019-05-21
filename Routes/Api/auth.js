2dbaabd3eb1607bb359096da13377c82d94c1613
Colburn Sanders <colburnsanders@gmail.com>
2019-05-21 13:00:46 -0500

    added middleware

--- Routes/Api/auth.js
+++ Routes/Api/auth.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../Middleware/auth');
+
+const User = require('../../Models/User');
 
 
 // @route   GET: api/auth
 // @des     Test route
 // @access  Public
-router.get('/', (req, res) => res.send('Auth route')); //test route
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password'); // leaves off the password.
+        res.json(user);
+    } catch (err) {
+        console.error(err.message)
+        res.status(501).send('Server error');
+    }
+});
 
 module.exports = router;
