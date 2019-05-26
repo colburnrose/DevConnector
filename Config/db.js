fef3193d534e82263c30c47f82a32cc78b761986
Colburn Sanders <colburnsanders@gmail.com>
2019-05-26 01:37:09 -0500

    create and update  profile routes

--- Config/db.js
+++ Config/db.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+
+
 const connectDB = async () => {
     try {
         await mongoose.connect(db, {
--- Routes/Api/profile.js
+++ Routes/Api/profile.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const Profile = require('../../Models/Profile');
 const User = require('../../Models/User');
 const auth = require('../../Middleware/auth');
+const {check,validationResult} = require('express-validator/check');
+const mongoose = require('mongoose');
+
+mongoose.set('useFindAndModify', false);
 
 
 // @route   GET: api/profile/me endpoint
@@ -26,4 +30,55 @@ router.get('/me', auth, async (req, res) => {
 });
 
 
+// @route   POST: api/profile
+// @des     Create or Update a Users Profile.
+// @access  Private
+router.post('/', [auth, [check('status', 'Status is required.').not().isEmpty(), check('skills', 'Skills is required').not().isEmpty()]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    const {company, website, location, bio, status, githubUserName, skills, youtube, facebook, twitter, instagram, linkedin}= req.body;
+
+    // Create profile obj
+    const profile = {};
+    profile.user = req.user.id;
+    if(company) profile.company = company;
+    if(website) profile.website = website;
+    if(location) profile.location = location;
+    if(bio) profile.bio = bio;
+    if(status) profile.status = status;
+    if(githubUserName) profile.githubUserName = githubUserName;
+    if(skills) {
+        profile.skills = skills.split(',').map(skill => skill.trim())
+    };
+
+    // Create social obj
+    profile.socialMedia = {};
+    if(youtube) profile.socialMedia.youtube = youtube;
+    if(twitter) profile.socialMedia.twitter = twitter;
+    if(facebook) profile.socialMedia.facebook = facebook;
+    if(linkedin) profile.socialMedia.linkedin = linkedin;
+    if(instagram) profile.socialMedia.instagram = instagram;
+
+    try {
+        let user = await Profile.findOne({user: req.user.id});
+
+        if(user) {
+            // Update
+            user = await Profile.findOneAndUpdate({user: req.user.id }, {$set: profile }, {new: true, useFindAndModify: false});
+            return res.json({user});
+        }
+
+        // Create
+        user = new Profile(profile);
+
+        await user.save();
+        res.json(user);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
