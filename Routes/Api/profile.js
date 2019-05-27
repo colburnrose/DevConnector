ec4ffbd1cf95cbeb27b08e4ee1a310f570ae79a8
Colburn Sanders <colburnsanders@gmail.com>
2019-05-26 20:33:49 -0500

    added experience

--- Routes/Api/profile.js
+++ Routes/Api/profile.js
@@ -129,4 +129,30 @@ router.delete('/', auth, async (req, res) => {
     }
 });
 
+
+// @route   PUT: api/profile/experience
+// @des     Add Profile experience
+// @access  Private
+router.put('/experience', [auth, [check('title', 'Title is required').not().isEmpty(),
+check('company', 'Company is required').not().isEmpty(), check('from', 'From date is required.').not().isEmpty()]], async (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    const {title, company, location, from, to, current, description} = req.body;
+    const newExp = {title, company, location, from, to, current, description}
+    
+    try {
+        const profile = await Profile.findOne({user: req.user.id}); //fetch the profile
+        profile.experience.unshift(newExp); // push experience to the beginning
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+});
+
 module.exports = router;
